756036a6c07437edfe6d263295782711a8c56f41
Sølve Tornøe <solve@tornoee.com>
2019-10-09 15:26:56 +0200

    Make room for old implementation of setCommits
    This makes it possible to use both nested and non-nested setOptions

--- src/index.js
+++ src/index.js
@@ -322,7 +322,7 @@ class SentryCliPlugin {
       })
       .then(() => this.cli.releases.uploadSourceMaps(release, this.options))
       .then(() => {
-        const { commit, previousCommit, repo, auto } = this.options.setCommits;
+        const { commit, previousCommit, repo, auto } = this.options.setCommits || this.options;
 
         if (repo && (commit || auto)) {
           this.cli.releases.setCommits(release, {
