3a14915f03f1bc29574fbb26963809f303beead9
Katie Byers <lobsterkatie@gmail.com>
2022-10-05 23:56:09 -0700

    fix integration test command for node 18+

--- example/test.js
+++ example/test.js
@@ -1,5 +1,23 @@
 /* eslint-disable no-console */
 const fs = require('fs');
+const childProcess = require('child_process');
+
+/**
+ * Run the given shell command, piping the shell process's `stdin`, `stdout`, and `stderr` to that of the current
+ * process. Returns contents of `stdout`.
+ */
+function run(cmd, options) {
+  return String(childProcess.execSync(cmd, { stdio: 'inherit', ...options }));
+}
+
+if (process.env.GITHUB_ACTIONS && process.env.NODE_VERSION === '18') {
+  // This is to avoid a `digital envelope routines unsupported` error which
+  // crops up when trying to combine Webpack 4 and Node 18, and must be set
+  // before webpack runs. See https://stackoverflow.com/a/69699772/.
+  process.env.NODE_OPTIONS += ' --openssl-legacy-provider';
+}
+
+run('yarn build');
 
 console.log();
 console.log('--------------------');
